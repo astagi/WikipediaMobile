44f1aaf063e8534ae3e240c1bca1597431c48f8f
4ndreaSt4gi <stagi.andrea@gmail.com>
2012-02-19 15:53:20 +0100

    Urlify text, translate elements added, better parsing

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -14,6 +14,7 @@ function aboutPage() {
 	chrome.hideOverlays();
 	$("#about-page-overlay").localize().show();
 	$("#aboutclose").unbind('click');
+	$("#about-license").html(urlifyText($("#about-license").html()));
 	getFile("CREDITS", writeCredits);
 	$("#aboutclose").bind('click', function(){
 		$("#about-page-overlay").hide();
@@ -23,12 +24,25 @@ function aboutPage() {
 }
 
 function writeCredits(data) {
+	data = urlifyText(data);
 	data = data.split("== Developers == ");
 	data = data[1].split("== Included external works ==");
-	data[0] = data[0].replace(/[\r\n]+/, "")
-	$("#about-contributors").html(data[0].replace(/[\r\n]+/g, ", "));
+	data[0] = data[0].replace(/[\r\n]+/, "");
+	data[0] = data[0].replace(/[\r\n]+/g, ", ");
+	data[0] = data[0].substr(0, data[0].length - 2);
+	data[1] = data[1].substr(data[1].search(/\*/));
+	data[1] = data[1].replace(/\*/g, "");
 	data[1] = data[1].replace(/[\r\n]+/g, "<br>")
+	$("#about-contributors").html(data[0]);
 	$("#about-software").html(data[1]);
+}
+
+function urlifyText(text)
+{
+    var matches = text.match(/http(s)?:\/\/[A-Za-z0-9\-\+&@#\/%?=~_|!:,\.;]+/g);
+    for(var i = 0 ; i < matches.length ; i++)
+        text = text.replace(matches[i], "<a href='" + matches[i] + "'>" + matches[i] + "</a>" );
+    return text;
 }
 
 function getFile(filepath, success_cb) {
